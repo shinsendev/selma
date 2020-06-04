18b7060a4cfa929d93493bdff85dc27d7c4ebaee
gdarquie <gaetan.darquie@gmail.com>
2020-06-04 13:20:02 +0200

    Category page style first version

--- src/pages/categories.tsx
+++ src/pages/categories.tsx
@@ -1,30 +1,37 @@
 import React from "react";
 import { graphql } from "gatsby";
 import Layout from "../components/layout";
-import { Paper, Container, Typography } from '@material-ui/core';
+import { Paper, Container, Typography, Grid } from "@material-ui/core";
+import "../styles/categoryPage.css";
 
 const CategoriesPage = ({data}) =>  {
     return (
         <Layout>
-            <Container maxWidth="md">
-                <Paper>
-                    <h2>Categories Page</h2>
-                    {data.mc3.categories.edges.map(({ node }, index) => (
-                      <Paper>
-                        <div>
-                          <h3>{node.title}</h3>
-                          <p>{node.model}</p>
-                          <p>{node.description}</p>
-                          <p>{node.attributesCount}</p>
-                          <div>
-                            {node.attributes.map(attribute => (
-                              <p>{attribute.title}</p>
-                            ))}
-                          </div>
-                        </div>
-                      </Paper>
+            <Container className='container' maxWidth="sm">
+                    {data.mc3.categories.edges.map(({ node }) => (
+                      <div>
+                        <h3 className='category-title'>{node.title} [{node.model}]</h3>
+
+                          <Paper elevation={0}>
+                            <section className='category-section'>
+                              <h4 className='property-title'>Description</h4>
+                              <p>{node.description}</p>
+                            </section>
+                          </Paper>
+
+                          <Paper elevation={0}>
+                            <section className='category-section'>
+                            <h4 className='property-title'>Attributes ({node.attributesCount})</h4>
+                            <ul>
+                              {node.attributes.map(attribute => (
+                                <li>{attribute.title}</li>
+                              ))}
+                            </ul>
+                            </section>
+                          </Paper>
+
+                      </div>
                     ))}
-                </Paper>
             </Container>
         </Layout>
     );
