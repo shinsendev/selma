f2d25a811627f9dc35115fab396c9a400ff1ebc8
gdarquie <gaetan.darquie@gmail.com>
2020-06-26 12:55:54 +0200

    WIP: work on numbers connected to a person

--- src/templates/person.tsx
+++ src/templates/person.tsx
@@ -40,6 +40,54 @@ const PersonPage = ( { pageContext: { person } }) => {
       ];
   }
 
+  function displayNumber(numbers) {
+    console.log(numbers);
+
+    if (numbers.length == 0) {
+      return;
+    }
+
+    return (
+      <Paper className='category-section numbers-paper' elevation={0}>
+        <h2 className='properties-title'>Numbers</h2>
+        <TableContainer component={Paper}>
+          <Table size="small" aria-label="Numbers list">
+            <TableHead>
+              <TableRow>
+                <TableCell>Position</TableCell>
+                <TableCell align="right">Title</TableCell>
+                <TableCell align="right">Film</TableCell>
+                <TableCell align="right">Released Year</TableCell>
+                <TableCell align="right">Professions</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {numbers.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((number) => (
+                <TableRow key={number.uuid} hover role="checkbox" tabIndex={-1}>
+              {/*    <TableCell component="th" scope="row">{number.order+1}</TableCell>*/}
+              {/*    <TableCell align="right"><Link to={/number/+number.uuid}>{number.title}</Link></TableCell>*/}
+              {/*    <TableCell align="right"><Property data={{content: number.beginTc, type: 'timecode'}} /></TableCell>*/}
+              {/*    <TableCell align="right"><Property data={{content: number.endTc, type: 'timecode'}} /></TableCell>*/}
+              {/*    <TableCell align="right"><Property data={{content: number.length, type: 'timecode'}} /></TableCell>*/}
+              {/*    <TableCell align="right"><Property data={{content: number.performers, type: 'list', options: {"listPropertyTitle": "fullname"}}} /></TableCell>*/}
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[10, 25, 100]}
+          component="div"
+          count={numbers.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onChangePage={handleChangePage}
+          onChangeRowsPerPage={handleChangeRowsPerPage}
+        />
+      </Paper>
+    )
+  }
+
   return (
     <Layout>
       <Container className='container' maxWidth="md">
@@ -51,44 +99,7 @@ const PersonPage = ( { pageContext: { person } }) => {
         <FilmsList title='Films' films={person.relatedFilms}></FilmsList>
 
         {/* numbers list  // if relatedNumbersByProfession // Fred Astaire uuid  = c6395587-ee36-4610-b87e-fd0e6fd40086 */}
-
-        <Paper className='category-section numbers-paper' elevation={0}>
-          <h2 className='properties-title'>Numbers</h2>
-          <TableContainer component={Paper}>
-            <Table size="small" aria-label="Numbers list">
-              <TableHead>
-                <TableRow>
-                  <TableCell>Position</TableCell>
-                  <TableCell align="right">Title</TableCell>
-                  <TableCell align="right">Film</TableCell>
-                  <TableCell align="right">Released Year</TableCell>
-                  <TableCell align="right">Professions</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {person.relatedNumbersByProfession.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((number) => (
-                  <TableRow key={number.uuid} hover role="checkbox" tabIndex={-1}>
-                    <TableCell component="th" scope="row">{number.order+1}</TableCell>
-                    <TableCell align="right"><Link to={/number/+number.uuid}>{number.title}</Link></TableCell>
-                    <TableCell align="right"><Property data={{content: number.beginTc, type: 'timecode'}} /></TableCell>
-                    <TableCell align="right"><Property data={{content: number.endTc, type: 'timecode'}} /></TableCell>
-                    <TableCell align="right"><Property data={{content: number.length, type: 'timecode'}} /></TableCell>
-                    <TableCell align="right"><Property data={{content: number.performers, type: 'list', options: {"listPropertyTitle": "fullname"}}} /></TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-          <TablePagination
-            rowsPerPageOptions={[10, 25, 100]}
-            component="div"
-            count={film.numbers.length}
-            rowsPerPage={rowsPerPage}
-            page={page}
-            onChangePage={handleChangePage}
-            onChangeRowsPerPage={handleChangeRowsPerPage}
-          />
-        </Paper>
+        {displayNumber(person.relatedNumbersByProfession)}
 
         {/* associated people list */}
 
