2fff608ba7fac005d3fe3657b03e17e731acccba
gdarquie <gaetan.darquie@gmail.com>
2020-07-10 09:56:54 +0200

    Minor change for search page

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -16,71 +16,73 @@ import {
 
 const SearchPage = () => {
 
-  console.log(process.env.ALGOLIA_ID);
   const searchClient = algoliasearch(
     process.env.ALGOLIA_ID,
     process.env.ALGOLIA_PWD
   );
 
-  const [searchInput, setSearchInput] = useState('');
-  const [results, setResults] = useState(''); // replace by an array
-
-  function displayResults() {
-
-    let results = "Result";
-
+  function displayNumber(number) {
     return (
-
-      <p>{results}</p>
+      <article>
+        <h2>{number.title} [{number.modelType}]</h2>
+        <p>{number.uuid} / {number.modelType}</p>
+        <p>begin tc :{number.startingTc} - ending tc: {number.endingTc}</p>
+        <p>{number.performers.map(performer => {
+          return (
+            <span>{performer.fullname}</span>
+          )
+        })}</p>
+      </article>
     )
   }
 
-  function search(e) {
-    let input = e.target.value;
-
-    // create an elastic search query and send it to the server
-    console.log(e.target.value);
-  }
-
   function Hit(props) {
-    // console.log(props.hit);
-    return (
-      <div>
-        <h2>{props.hit.title}</h2>
-        <h2>{props.hit.fullname}</h2>
-        <p>{props.releasedYear}</p>
-        {props.hit.uuid}
-      </div>
-    );
+
+    console.log(props.hit.modelType);
+
+    switch (props.hit.modelType) {
+      case 'number':
+        return displayNumber(props.hit);
+        break;
+      case 'film':
+        console.log('this is a film');
+        break;
+      case 'person':
+        console.log('this is a person');
+      case 'song':
+        console.log('this is a song');
+      default:
+        console.log('Error, unrecognized modelType .');
+    }
+
+    return null
   }
   
   return (
     <Layout>
       <Container className='container'  maxWidth="lg">
         <form noValidate autoComplete="off" className="searchForm">
-          <TextField id="main-search-field" label="MC2 Search engine" onKeyUp={search}/>
+          <TextField id="main-search-field" label="MC2 Search engine"/>
         </form>
 
-        <section className="results-wrappers">
-          <div className="results">
-            {displayResults()}
-          </div>
-        </section>
-
         <div className="ais-InstantSearch">
-          <h1>React InstantSearch e-commerce demo</h1>
+          <h1>MC2 search page</h1>
+
           <InstantSearch indexName="mc2" searchClient={searchClient}>
+
             <div className="left-panel">
               <ClearRefinements />
-              <h2>Brands</h2>
-              <RefinementList attribute="brand" />
-              <Configure hitsPerPage={8} />
+              <h2>Types</h2>
+              <RefinementList attribute="modelType" />
+              <Configure hitsPerPage={25} />
             </div>
+
             <div className="right-panel">
               <SearchBox />
               <Hits hitComponent={Hit} />
               <Pagination />
             </div>
+
           </InstantSearch>
         </div>
       </Container>
