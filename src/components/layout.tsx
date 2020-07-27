81b67ee565c99aad45c50d964bb06e3663c3a52d
gdarquie <gaetan.darquie@gmail.com>
2020-07-27 19:52:16 +0200

    Redesign footer and add some refacto

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./header";
+import Footer from "./footer";
 import { createMuiTheme, ThemeProvider, Theme, makeStyles, createStyles } from '@material-ui/core/styles';
-import { Typography } from '@material-ui/core';
 
 
 const theme = createMuiTheme({
@@ -20,35 +20,15 @@ const theme = createMuiTheme({
   },
 });
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    footer: {
-      color: 'white',
-      background: '#4d5d5d',
-      height: '175px',
-      paddingTop: '0.2em',
-      paddingLeft: '1em',
-      paddingRight: '1em',
-      width: '100%%',
-      margin: 'auto',
-      position:'relative',
-    }
-  })
-);
+
 
 export default function BaseLayout({ children }) {
-    const styles = useStyles();
     return (
         <ThemeProvider theme={theme}>
             <div>
                 <Header/>
-
                 {children}
-
-                <section className = {styles.footer}>
-                  <h2>Partnership</h2>
-                  <Typography>This project is supported by the Labex Arts-H2H and benefits from financial support from the ANR as part of the “Investments for the future” program (ANR-10-LABX-80-01).</Typography>
-                </section>
+                <Footer/>
             </div>
         </ThemeProvider>
   )
