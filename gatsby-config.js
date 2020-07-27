31295c2431675c317f3db740c6594734de701f24
gdarquie <gaetan.darquie@gmail.com>
2020-07-27 17:04:42 +0200

    Add loader

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,5 +26,14 @@ module.exports = {
         bucketName: "mc3-website",
       },
     },
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        // Setting a color is optional.
+        color: `tomato`,
+        // Disable the loading spinner.
+        showSpinner: false,
+      },
+    },
   ],
 }
