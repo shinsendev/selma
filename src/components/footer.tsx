cdbe7df845b1b917adc8cfdc62445902d2b02ea9
gdarquie <gaetan.darquie@gmail.com>
2020-07-28 19:44:01 +0200

    Minor change for footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -51,8 +51,8 @@ const Footer = () => {
 
         <Grid item xs={12} md={6}>
           <Typography variant='h4'>Development</Typography>
-          <Typography>This project has been coded by Gaétan Darquié from <Link className={styles.customLink} to='http://shinsen.fr'>Shinsen Dev</Link>.</Typography>
-          <Typography>The project is open source: the frontend code is available in <Link target='_blank' to='https://github.com/shinsendev/selma'  className={styles.customLink}>selma repository</Link> and the  backend api code in <Link target='_blank' to='https://github.com/shinsendev/mc3' className={styles.customLink}>mc3 repository</Link>.</Typography>
+          <Typography>This project has been crafted by Gaétan Darquié from <Link className={styles.customLink} to='http://shinsen.fr'>Shinsen Dev</Link>.</Typography>
+          <Typography>The code is open source: the frontend code is available in <Link target='_blank' to='https://github.com/shinsendev/selma'  className={styles.customLink}>selma repository</Link> and the  backend api code in <Link target='_blank' to='https://github.com/shinsendev/mc3' className={styles.customLink}>mc3 repository</Link>.</Typography>
         </Grid>
 
         <Grid item xs={12} md={6}>
