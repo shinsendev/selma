7d5138c539391bd9f8c84afe259022c60dcadc7b
gdarquie <gaetan.darquie@gmail.com>
2020-07-30 15:35:13 +0200

    minor correction

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,5 @@
 import { action } from "@storybook/addon-actions"
+import '../src/styles/global.css';
 
 // Gatsby's Link overrides:
 // Gatsby Link calls the `enqueue` & `hovering` methods on the global variable ___loader.
