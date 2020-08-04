9bdf52e89aca93647bbf937e2697dd130b7d4eef
gdarquie <gaetan.darquie@gmail.com>
2020-08-04 21:37:06 +0200

    Clean PR

--- src/components/organisms/Chronology.tsx
+++ src/components/organisms/Chronology.tsx
@@ -2,42 +2,12 @@ import React, { useState } from "react";
 import { Chart, Series } from 'devextreme-react/chart';
 
 const Chronology = (data) => {
-  const dataSource = [{
-    year: 1962,
-    count: 3
-  }, {
-    year: 1963,
-    count: 2
-  }, {
-    year: 1966,
-    count: 3
-  }, {
-    year: 1968,
-    count: 4
-  }, {
-    year: 1970,
-    count: 6
-  }, {
-    year: 1971,
-    count: 11
-  }, {
-    year: 1972,
-    count: 4
-  }];
-
 
   function getData(data):any[] {
 
     let computedData = [];
 
     data.map(element => {
-      // console.log(element.years);
-      //
-      // computedData.push({
-      //   "year": element.years[0],
-      //   "count": 1,
-      // })
-
       if (computedData[element.years[0]] > 0) {
         computedData[element.years[0]] = computedData[element.years[0]] + 1;
 
